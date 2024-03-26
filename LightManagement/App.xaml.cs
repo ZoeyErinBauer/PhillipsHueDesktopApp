d5ea21d5ccaa49f4eeae8bd8c0aae6d5bce698c6
Zoey <zbauer@checkintocash.com>
2024-03-25 20:36:31 -0400

    Add new platform-specific files and update application UI assets
    This commit adds new platform-specific files to Windows, Android and MacCatalyst folders, including essential configuration and UI files. It also includes new SVG files for app icons and splash screen, alongside vital Maui application files like App.xaml and .razor files for routing. This expands the compatibility and UI elements of the LightManagement system.

---
+++ LightManagement/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace LightManagement;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new MainPage();
+    }
+}
---
+++ LightManagement/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace LightManagement;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
